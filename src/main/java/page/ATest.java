1838a2dca247955ec6abb42d117dac7cf34ba60a
Juhi <sinhajuhi9519@gmail.com>
2022-11-14 19:15:47 +0530

    my first commit

---
+++ src/main/java/page/ATest.java
@@ -0,0 +1,12 @@
+package page;
+import org.testng.annotations.Test;
+
+public class ATest {
+@Test
+	public  void main() {
+	System.out.println("page/ATest juhi A");
+
+	}
+
+
+}
---
+++ src/test/java/test/BTest.java
@@ -0,0 +1,11 @@
+package test;
+import org.testng.annotations.Test;
+
+public class BTest {
+@Test
+	public  void main() {
+	System.out.println("test/btest juhi TestB");
+
+	}
+
+}
