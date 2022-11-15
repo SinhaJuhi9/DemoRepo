4a5dd5614fed382f6a1de4465c583275a90bfca4
Juhi <sinhajuhi9519@gmail.com>
2022-11-15 19:13:16 +0530

    us timezone commit

--- src/main/java/page/ATest.java
+++ src/main/java/page/ATest.java
@@ -3,10 +3,13 @@ import org.testng.annotations.Test;
 
 public class ATest {
 @Test
-	public  void main() {
+	public  void main1() {
 	System.out.println("page/ATest juhi A");
 
 	}
 
+public  void main2() {
+	System.out.println("page/ATest juhi A main 2");
 
+	}
 }
